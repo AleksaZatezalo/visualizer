9ff347c1b4dbc42db8be08ae7e64da416a0953c2
aleksa <zabumaphu@gmail.com>
2024-08-16 20:50:50 -0400

    SDL Setup Complete.

--- drawnet.c
+++ drawnet.c
@@ -1,6 +1,45 @@
 #include "drawnet.h"
+#define WINDOW_X    10
+#define WINDOW_Y    -20
+#define WINDOW_WIDTH    1770
+#define WINDOW_HEIGHT 1410
 
-int main(){
+int main()
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    if(SDL_INIT_VIDEO < 0 ){
+        fprintf(stderr, "ERROR: SDL_INIT_VIDEO");
+    }
+
+    window = SDL_CreateWindow(
+        "",
+        WINDOW_X,
+        WINDOW_Y,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if(!window){
+        fprintf(stderr, "ERROR: !window");
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if(!renderer){
+        fprintf(stderr, "ERROR: !renderer");
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0x00,0xFF,0x00,100);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
